ce9d74de52328c0bfa71efd239655b969b57c660
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-11-15 15:11:39 +0700

    review product

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,3 +1,4 @@
+// UPDATED
 const reviewService = require('../services/review.service');
 const BaseController = require('../utils/BaseController');
 
@@ -6,6 +7,10 @@ class ReviewController extends BaseController {
     super(reviewService);
   }
 
+  /**
+   * POST /api/reviews
+   * Create review (restaurant or product)
+   */
   create = async (req, res, next) => {
     try {
       const errors = this.validateRequest(req);
@@ -34,6 +39,38 @@ class ReviewController extends BaseController {
     }
   };
 
+  /**
+   * GET /api/reviews/type/:type
+   * Get all reviews by type (restaurant or product)
+   */
+  getByType = async (req, res, next) => {
+    try {
+      const { type } = req.params;
+
+      const result = await this.service.getReviewsByType(type, req.parsedQuery);
+
+      if (!result.success) {
+        return res.status(result.statusCode || 400).json({
+          success: false,
+          message: result.message
+        });
+      }
+
+      res.json({
+        success: true,
+        count: result.data.length,
+        data: result.data,
+        pagination: result.pagination
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   * GET /api/reviews/restaurant/:restaurantId
+   * Get restaurant reviews
+   */
   getRestaurantReviews = async (req, res, next) => {
     try {
       const result = await this.service.getRestaurantReviews(
@@ -52,6 +89,32 @@ class ReviewController extends BaseController {
     }
   };
 
+  /**
+   * GET /api/reviews/product/:productId
+   * Get product reviews
+   */
+  getProductReviews = async (req, res, next) => {
+    try {
+      const result = await this.service.getProductReviews(
+        req.params.productId,
+        req.parsedQuery
+      );
+
+      res.json({
+        success: true,
+        count: result.data.length,
+        data: result.data,
+        pagination: result.pagination
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   * GET /api/reviews/user/me
+   * Get my reviews
+   */
   getMyReviews = async (req, res, next) => {
     try {
       const result = await this.service.getMyReviews(req.user.id, req.parsedQuery);
@@ -67,6 +130,73 @@ class ReviewController extends BaseController {
     }
   };
 
+  /**
+   * GET /api/reviews/user/stats
+   * Get review statistics
+   */
+  getStats = async (req, res, next) => {
+    try {
+      const result = await this.service.getReviewStats(req.user.id);
+
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   * GET /api/reviews/check/:type/:targetId
+   * Check if user has reviewed
+   */
+  checkReview = async (req, res, next) => {
+    try {
+      const { type, targetId } = req.params;
+
+      const result = await this.service.checkReview(
+        req.user.id,
+        type,
+        targetId
+      );
+
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   * PUT /api/reviews/:id
+   * Update review
+   */
+  update = async (req, res, next) => {
+    try {
+      const errors = this.validateRequest(req);
+      if (errors) {
+        return res.status(400).json({
+          success: false,
+          errors
+        });
+      }
+
+      const result = await this.service.update(req.params.id, req.body);
+
+      if (!result.success) {
+        return res.status(result.statusCode || 400).json({
+          success: false,
+          message: result.message
+        });
+      }
+
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   * DELETE /api/reviews/:id
+   * Delete review
+   */
   delete = async (req, res, next) => {
     try {
       const result = await this.service.deleteReview(
@@ -83,6 +213,25 @@ class ReviewController extends BaseController {
       }
 
       res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   * GET /api/reviews (Admin only)
+   * Get all reviews
+   */
+  getAll = async (req, res, next) => {
+    try {
+      const result = await this.service.findAll(req.parsedQuery);
+
+      res.json({
+        success: true,
+        count: result.data.length,
+        data: result.data,
+        pagination: result.pagination
+      });
     } catch (error) {
       next(error);
     }
